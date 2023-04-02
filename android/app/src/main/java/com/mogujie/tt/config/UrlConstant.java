79fdb6dcc88996faa35b6f2335f8b7b5249b15cc
gitopening <97137@qq.com>
2023-04-02 12:49:13 +0800

    first

--- android/app/src/main/java/com/mogujie/tt/config/UrlConstant.java
+++ android/app/src/main/java/com/mogujie/tt/config/UrlConstant.java
@@ -15,7 +15,7 @@ public class UrlConstant {
 //    public final static String ACCESS_MSG_ADDRESS = "http://im.xiaominfc.com:8080/msg_server";
 //    public final static String ACCESS_MSG_ADDRESS = "http://192.168.0.109:8080/msg_server";
 //    public final static String ACCESS_MSG_ADDRESS = "http://192.168.0.107:8080/msg_server";
-    public final static String ACCESS_MSG_ADDRESS = "http://139.10.10.1:8080/msg_server";
+    public final static String ACCESS_MSG_ADDRESS = "http://192.168.0.37:8080/msg_server";
 //      <string name="str_sign_switch_login_server_xmfc">http://im.xiaominfc.com:8080/msg_server</string>
 
 }
